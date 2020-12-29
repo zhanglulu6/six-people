205c27a5f21ac42dc6048c62572ba7c65474585d
maosuling <1679898641@qq.com>
2020-12-29 15:33:51 +0800

    maosuling

--- src/store/deepplay/actions.ts
+++ src/store/deepplay/actions.ts
@@ -3,7 +3,7 @@ import { getDeepPlayApi } from "../../utils/api";
 export default {
   async getDeepPlayApi({ commit }:any) {
     const res = await getDeepPlayApi({});
-    console.log(res);
+    // console.log(res);
     commit("changeDeepPlayList", res.list);
     // console.log(res.list);
   }
